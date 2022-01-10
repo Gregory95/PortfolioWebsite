3b71aa67ef7f928dca9d51083c5dd8df8c0319e5
Grigoris <kokozides.g@outlook.com>
2022-01-10 21:00:08 +0200

    Delete Uneccessary files

--- src/components/landingPage-component.jsx
+++ src/components/landingPage-component.jsx
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 
 class LandingPage extends Component {
 
-    render() { 
-        return <div></div>;
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
     }
 }
- 
+
 export default LandingPage;
